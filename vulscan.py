1167d60b13a26e2dc0c1cf0afbb014c783f073e4
ilyas mutlu <79372941+ilyaasmutlu@users.noreply.github.com>
2021-03-01 19:16:04 +0300

    Create vulscan.py

---
+++ vulscan.py
@@ -0,0 +1,4 @@
+import portscanner
+
+targets_ip = input('[+] Enter Target To Scan For Vulnerable Open Ports: ')
+port_number = int(input('[+] Enter Amount Of Ports You Want To Scan(500 - first 500 ports): '))
