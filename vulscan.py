e719fcff69267efab1adb8898bbd07f0522cf6b8
ilyas mutlu <79372941+ilyaasmutlu@users.noreply.github.com>
2021-03-02 21:38:35 +0300

    Update vulscan.py

--- vulscan.py
+++ vulscan.py
@@ -5,7 +5,18 @@ port_number = int(input('[+] Enter Amount Of Ports You Want To Scan(500 - first 
 vul_file = input('[+] Enter Path To The File With Vulnerable Softwares: ')
 print('\n')
 
-portscanner.scan(targets_ip)
+target = portscanner.PortScan(targets_ip, port_number)
+target.scan()
+
+with open(vul_file, 'r') as file:
+    count = 0
+    for banner in target.banners:
+        file.seek(0)
+        for line in file.readlines():
+            if line.strip() in banner:
+                print('[!] VULNEREBLE BANNER' + banner + 'ON PORT' + str(target.open_ports[count]))
+
+        count += 1
 
 
 
