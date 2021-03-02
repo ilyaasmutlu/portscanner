8748e4d8bf64d65892d84f9aa76693cdccd6af93
ilyas mutlu <79372941+ilyaasmutlu@users.noreply.github.com>
2021-03-02 21:39:15 +0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -3,18 +3,15 @@ from IPy import IP
 
 
 class PortScan():
-    banners =[]
+    banners = []
     open_ports = []
     def __init__ (self,target,port_num):
         self.target = target
         self.port_num  =  port_num
 
-
-
     def scan(self,target):
         for port in range(1,500):
             self.scan_port(port)
-
 
     def check_ip(self):
         try:
@@ -23,23 +20,20 @@ class PortScan():
         except ValueError:
             return socket.gethostbyname(self.target)
 
-
-
-
     def scan_port(self,port):
         try:
             converted_ip = self.check_ip()
             sock = socket.socket()
             sock.settimeout(0.05)
             sock.connect((converted_ip, port))
+            self.open_ports.append(port)
             try:
                 banner = sock.recv(1024).decode().strip('\n').strip('\r')
                 self.banners.append(banner)
             except:
-                pass
+                self.banners.append(' ')
             sock.close()
-
         except:
             pass
 
-    
+
